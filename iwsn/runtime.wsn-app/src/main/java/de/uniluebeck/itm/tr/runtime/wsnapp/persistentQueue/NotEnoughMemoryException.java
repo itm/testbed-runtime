7095d03f917ee10a807e191573b501fdeea19205
Nils Rohwedder <nrohwedder@Nils-Rohwedders-MacBook-Pro.local>
2010-08-11 14:39:22 +0200

    PersistenceQueue added

---
+++ iwsn/runtime.wsn-app/src/main/java/de/uniluebeck/itm/tr/runtime/wsnapp/persistentQueue/NotEnoughMemoryException.java
@@ -0,0 +1,11 @@
+package de.uniluebeck.itm.tr.runtime.wsnapp.persistentQueue;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nrohwedder
+ * Date: 11.08.2010
+ * Time: 11:45:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class NotEnoughMemoryException extends Throwable {
+}
---
+++ iwsn/runtime.wsn-app/src/main/java/de/uniluebeck/itm/tr/runtime/wsnapp/persistentQueue/PersistentQueue.java
@@ -0,0 +1,28 @@
+package de.uniluebeck.itm.tr.runtime.wsnapp.persistentQueue;
+
+import de.uniluebeck.itm.tr.runtime.wsnapp.persistentQueue.NotEnoughMemoryException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nrohwedder
+ * Date: 10.08.2010
+ * Time: 16:38:20
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PersistentQueue <E extends Serializable> {
+
+    public boolean add(E object) throws NotEnoughMemoryException, LongOverflowException;
+
+    public E poll() throws IOException, ClassNotFoundException;
+
+    public E peek() throws IOException, ClassNotFoundException;
+
+    public boolean isEmpty();
+
+    public long size();
+    
+}
