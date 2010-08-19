9b222046e64b1518dd733a3ea19b951a36ab69e4
Nils Rohwedder <nrohwedder@Nils-Rohwedders-MacBook-Pro.local>
2010-08-19 10:31:56 +0200

    ticket #39 added

---
+++ iwsn/runtime.wsn-app/src/main/java/de/uniluebeck/itm/tr/runtime/wsnapp/WSNDeviceAppGuavaImpl.java
@@ -0,0 +1,11 @@
+package de.uniluebeck.itm.tr.runtime.wsnapp;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nrohwedder
+ * Date: 18.08.2010
+ * Time: 14:54:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class WSNDeviceAppGuavaImpl {
+}
