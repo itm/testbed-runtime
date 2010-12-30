e5d46b90043b0714bb9b16fe1a08577e1498c9ba
Daniel Bimschas <daniel@bimschas.com>
2010-12-30 11:47:14 +0100

    Temporary bugfix for #139 (strip invalid XML characters out before sending TextMessage object to client)

--- iwsn/wsn-api/src/main/java/eu/wisebed/testbed/api/wsn/v211/TextMessage.java
+++ iwsn/wsn-api/src/main/java/eu/wisebed/testbed/api/wsn/v211/TextMessage.java
@@ -59,7 +59,7 @@ public class TextMessage {
      *     
      */
     public void setMsg(String value) {
-        this.msg = value;
+        this.msg = stripNonValidXMLCharacters(value);
     }
 
     /**
@@ -84,6 +84,35 @@ public class TextMessage {
      */
     public void setMessageLevel(MessageLevel value) {
         this.messageLevel = value;
+    }
+
+	/**
+     * This method ensures that the output String has only
+     * valid XML unicode characters as specified by the
+     * XML 1.0 standard. For reference, please see
+     * <a href="http://www.w3.org/TR/2000/REC-xml-20001006#NT-Char">the
+     * standard</a>. This method will return an empty
+     * String if the input is null or empty.
+     *
+     * @param in The String whose non-valid characters we want to remove.
+     * @return The in String, stripped of non-valid characters.
+     */
+    private String stripNonValidXMLCharacters(String in) {
+        StringBuffer out = new StringBuffer(); // Used to hold the output.
+        char current; // Used to reference the current character.
+
+        if (in == null || ("".equals(in))) return ""; // vacancy test.
+        for (int i = 0; i < in.length(); i++) {
+            current = in.charAt(i); // NOTE: No IndexOutOfBoundsException caught here; it should not happen.
+            if ((current == 0x9) ||
+                (current == 0xA) ||
+                (current == 0xD) ||
+                ((current >= 0x20) && (current <= 0xD7FF)) ||
+                ((current >= 0xE000) && (current <= 0xFFFD)) ||
+                ((current >= 0x10000) && (current <= 0x10FFFF)))
+                out.append(current);
+        }
+        return out.toString();
     }
 
 }
