8c18c4d0ecc4ee8ed1f8a637fe5daf088e7f9b39
Daniel Bimschas <daniel@bimschas.com>
2011-03-17 14:13:14 +0100

    Updated version number of WSN.getVersion()

--- iwsn/wsn-api/src/main/java/eu/wisebed/testbed/api/wsn/Constants.java
+++ iwsn/wsn-api/src/main/java/eu/wisebed/testbed/api/wsn/Constants.java
@@ -25,7 +25,7 @@ package eu.wisebed.testbed.api.wsn;
 
 public class Constants {
 
-	public static final String VERSION = "2.1.1";
+	public static final String VERSION = "2.2";
 
 	public static final String NAMESPACE_COMMON_TYPES = "urn:CommonTypes";
 
