e5fea9009076b37d35eb8c227f35e55e3e7ce641
Daniel Bimschas <daniel@bimschas.com>
2011-03-17 13:50:10 +0100

    Fixed OneJar packaging for SNAA (removing Google Guice Finalizer exception on startup)

---
+++ wisebed-cmdline-client/src/main/java/de/uniluebeck/itm/wisebed/cmdlineclient/ScriptingPlatformTest.java
@@ -0,0 +1,8 @@
+package de.uniluebeck.itm.wisebed.cmdlineclient;
+
+/**
+ * Created by IntelliJ IDEA. User: bimschas Date: 17.03.11 Time: 13:06 TODO change
+ */
+public class ScriptingPlatformTest {
+
+}
