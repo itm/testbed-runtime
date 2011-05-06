859a3af59383c6310954031cb356b4f6b77864be
Daniel Bimschas <daniel@bimschas.com>
2011-05-06 16:50:14 +0200

    Cleaned up script client

--- clients/scripting-client/scripts/federationdemo/clean-db.sql
+++
@@ -1,7 +0,0 @@
-delete from ConfidentialReservationDataInternal;
-delete from ConfidentialReservationDataInternal_Users;
-delete from PublicReservationDataInternal;
-delete from ReservationDataInternal;
-delete from SecretReservationKeyInternal;
-delete from Users;
-delete from reservationdata_urns;
