89928f0ce9c1c697df80e1a97b0e32088fb8e492
Daniel Bimschas <daniel@bimschas.com>
2011-05-07 15:06:25 +0200

    Removed logcontroller and messagestore-api module

--- iwsn/messagestore-api/wsimport.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Generating classes for MessageStoreService..."
-wsimport -p eu.wisebed.testbed.api.messagestore.v1 -s src/main/java/ -keep -wsdllocation REPLACE_WITH_ACTUAL_URL -Xnocompile src/main/resources/MessageStoreService.wsdl
