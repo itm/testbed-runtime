795cfb3801c43f57afdc0df02dd54dadb84f4576
Daniel Bimschas <daniel@bimschas.com>
2011-12-28 15:32:14 +0100

    Removed deployment scripts that were started but never completed

--- assemblies/deployment-scripts/src/main/assembly/collect-logs-iwsn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ARGS_REQ=1
-ARGS_ADDITIONAL=""
-source include/choose-testbed
-source include/collect-logs-iwsn
--- assemblies/deployment-scripts/src/main/assembly/distribute-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ARGS_REQ=2
-ARGS_ADDITIONAL="VERSION"
-source include/choose-testbed
-TR_VERSION=$2
-source include/distribute-config
