6464b4a14c7d685db70027ab8714359c2347cc6b
Daniel Bimschas <daniel@bimschas.com>
2013-04-04 18:09:30 +0200

    Further working on integration of REST API

--- iwsn-portal/src/main/java/de/uniluebeck/itm/tr/iwsn/portal/api/rest/v1/resources/SnaaResource.java
+++ iwsn-portal/src/main/java/de/uniluebeck/itm/tr/iwsn/portal/api/rest/v1/resources/SnaaResource.java
@@ -5,8 +5,7 @@ import de.uniluebeck.itm.tr.iwsn.portal.api.rest.v1.dto.LoginData;
 import de.uniluebeck.itm.tr.iwsn.portal.api.rest.v1.dto.SnaaSecretAuthenticationKeyList;
 import de.uniluebeck.itm.tr.iwsn.portal.api.rest.v1.util.Base64Helper;
 import eu.wisebed.api.v3.common.SecretAuthenticationKey;
-import eu.wisebed.api.v3.snaa.Action;
-import eu.wisebed.api.v3.snaa.SNAA;
+import eu.wisebed.api.v3.snaa.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +16,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import static de.uniluebeck.itm.tr.iwsn.portal.api.rest.v1.resources.ResourceHelper.getSAKsFromCookie;
+import static de.uniluebeck.itm.tr.iwsn.portal.api.rest.v1.util.JSONHelper.toJSON;
 
 
 @Path("/")
@@ -42,10 +42,15 @@ public class SnaaResource {
 
 		try {
 
-			boolean isLoggedIn = snaa.isValid(secretAuthenticationKeys);
-			return isLoggedIn ? Response.ok().build() : Response.status(Status.FORBIDDEN).build();
+			for (ValidationResult validationResult : snaa.isValid(secretAuthenticationKeys)) {
+				if (!validationResult.isValid()) {
+					return Response.status(Status.FORBIDDEN).build();
+				}
+			}
 
-		} catch (SNAAExceptionException e) {
+			return Response.ok().build();
+
+		} catch (SNAAFault_Exception e) {
 			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e).build();
 		}
 	}
@@ -71,8 +76,6 @@ public class SnaaResource {
 	 * }
 	 * </code>
 	 *
-	 * @param testbedId
-	 * 		the ID of the testbed
 	 * @param loginData
 	 * 		login data
 	 *
@@ -82,27 +85,23 @@ public class SnaaResource {
 	@Produces({MediaType.APPLICATION_JSON})
 	@Consumes({MediaType.APPLICATION_JSON})
 	@Path("login")
-	public Response login(@PathParam("testbedId") final String testbedId, final LoginData loginData) {
-
-		List<SecretAuthenticationKey> secretAuthenticationKeys;
+	public Response login(final LoginData loginData) {
 
 		try {
 
-			SNAA snaa = endpointManager.getSnaaEndpoint(testbedId);
-
-			secretAuthenticationKeys = snaa.authenticate(loginData.authenticationData);
+			List<SecretAuthenticationKey> secretAuthenticationKeys = snaa.authenticate(loginData.authenticationData);
 			SnaaSecretAuthenticationKeyList loginResult = new SnaaSecretAuthenticationKeyList(secretAuthenticationKeys);
 			String jsonResponse = toJSON(loginResult);
 
 			List<NewCookie> cookies = new LinkedList<NewCookie>();
-			cookies.add(createCookie(testbedId, loginResult, "", false));
+			cookies.add(createCookie(loginResult, "", false));
 
 			log.trace("Received {}, returning {}", toJSON(loginData), jsonResponse);
 			return Response.ok(jsonResponse).cookie(cookies.toArray(new NewCookie[cookies.size()])).build();
 
-		} catch (AuthenticationExceptionException e) {
+		} catch (AuthenticationFault_Exception e) {
 			return createLoginErrorResponse(e);
-		} catch (SNAAExceptionException e) {
+		} catch (SNAAFault_Exception e) {
 			return createLoginErrorResponse(e);
 		}
 
@@ -111,16 +110,16 @@ public class SnaaResource {
 	@GET
 	@Path("logout")
 	public Response deleteCookie(@PathParam("testbedId") final String testbedId) {
-		return Response.ok().cookie(createCookie(testbedId, null, "", true)).build();
+		return Response.ok().cookie(createCookie(null, "", true)).build();
 	}
 
-	private NewCookie createCookie(final String testbedId, SnaaSecretAuthenticationKeyList loginData, String domain,
-								   boolean isReset) {
+	private NewCookie createCookie(SnaaSecretAuthenticationKeyList loginData, String domain, boolean isReset) {
+
 		int maxAge = isReset ? 0 : 60 * 60 * 24;
 		boolean secure = false;
 		String comment = "";
 		String value = (loginData == null) ? "" : Base64Helper.encode(toJSON(loginData));
-		String name = createSecretAuthenticationKeyCookieName(testbedId);
+		String name = Constants.COOKIE_SECRET_AUTH_KEY;
 		String path = "/";
 
 		return new NewCookie(name, value, path, domain, comment, maxAge, secure);
