37dc96a122618cd4b15d8e8f79d71d6741f314e0
Daniel Bimschas <daniel@bimschas.com>
2013-05-14 11:10:28 +0200

    Moved SNAA client in its own module

--- snaa-client/src/main/java/de/uniluebeck/itm/tr/snaa/client/SNAAClient.java
+++ snaa-client/src/main/java/de/uniluebeck/itm/tr/snaa/client/SNAAClient.java
@@ -21,7 +21,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                *
  **********************************************************************************************************************/
 
-package de.uniluebeck.itm.tr.snaa;
+package de.uniluebeck.itm.tr.snaa.client;
 
 import de.uniluebeck.itm.tr.util.Logging;
 import eu.wisebed.api.v3.WisebedServiceHelper;
