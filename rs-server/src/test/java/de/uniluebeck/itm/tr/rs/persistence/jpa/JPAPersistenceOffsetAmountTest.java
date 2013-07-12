7e527a65a9e0d8d4c69148ed8ea03469364e4e73
Daniel Bimschas <daniel@bimschas.com>
2013-07-12 14:23:21 +0200

    Fixing case-insensitive filesystem bug (again)

--- rs-server/src/test/java/de/uniluebeck/itm/tr/rs/persistence/jpa/JPAPersistenceOffsetAmountTest.java
+++ rs-server/src/test/java/de/uniluebeck/itm/tr/rs/persistence/jpa/JPAPersistenceOffsetAmountTest.java
@@ -13,7 +13,7 @@ import java.util.Properties;
 import java.util.TimeZone;
 
 @RunWith(MockitoJUnitRunner.class)
-public class JPARSPersistenceOffsetAmountTest extends RSPersistenceOffsetAmountTest {
+public class JPAPersistenceOffsetAmountTest extends RSPersistenceOffsetAmountTest {
 
 	private static final Map<String, String> properties = new HashMap<String, String>() {{
 
