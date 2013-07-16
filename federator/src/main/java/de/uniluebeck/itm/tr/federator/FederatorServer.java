a494e27e2f68158b1aa12cd5c814d65a84fe4a4e
Daniel Bimschas <daniel@bimschas.com>
2013-07-16 16:52:14 +0200

    Updated federator-deb configuration according to optimized shaded JAR structure #294

--- federator/src/main/java/de/uniluebeck/itm/tr/federator/FederatorServer.java
+++ federator/src/main/java/de/uniluebeck/itm/tr/federator/FederatorServer.java
@@ -8,6 +8,8 @@ import de.uniluebeck.itm.tr.common.config.ConfigWithLoggingAndProperties;
 import de.uniluebeck.itm.tr.federator.iwsn.IWSNFederatorServiceConfig;
 import de.uniluebeck.itm.tr.federator.rs.RSFederatorServiceConfig;
 import de.uniluebeck.itm.tr.federator.snaa.SNAAFederatorServiceConfig;
+import de.uniluebeck.itm.util.logging.LogLevel;
+import de.uniluebeck.itm.util.logging.Logging;
 import de.uniluebeck.itm.util.propconf.PropConfModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +20,10 @@ import static de.uniluebeck.itm.tr.common.config.ConfigHelper.setLogLevel;
 import static de.uniluebeck.itm.util.propconf.PropConfBuilder.printDocumentationAndExit;
 
 public class FederatorServer extends AbstractService {
+
+	static {
+		Logging.setLoggingDefaults(LogLevel.WARN);
+	}
 
 	public static final Logger log = LoggerFactory.getLogger(FederatorServer.class);
 
