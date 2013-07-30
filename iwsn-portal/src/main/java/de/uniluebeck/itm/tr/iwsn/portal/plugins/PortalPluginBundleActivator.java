47ca34339048f865711e56ad9ea4dd465ea08a7a
Daniel Bimschas <daniel@bimschas.com>
2013-07-30 09:50:43 +0200

    Working on #303 (Embed OSGi environment to execute plugins on portal server)
    Providing Guice module to plugins so the can use DI for their services.

--- iwsn-portal/src/main/java/de/uniluebeck/itm/tr/iwsn/portal/plugins/PortalPluginBundleActivator.java
+++ iwsn-portal/src/main/java/de/uniluebeck/itm/tr/iwsn/portal/plugins/PortalPluginBundleActivator.java
@@ -1,5 +1,9 @@
 package de.uniluebeck.itm.tr.iwsn.portal.plugins;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
 import de.uniluebeck.itm.tr.common.plugins.PluginContainer;
 import de.uniluebeck.itm.tr.devicedb.DeviceDBService;
 import de.uniluebeck.itm.tr.iwsn.common.ResponseTrackerFactory;
@@ -11,6 +15,8 @@ import eu.wisebed.api.v3.snaa.SNAA;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+
+import static com.google.inject.util.Providers.of;
 
 public abstract class PortalPluginBundleActivator implements BundleActivator {
 
@@ -47,7 +53,6 @@ public abstract class PortalPluginBundleActivator implements BundleActivator {
 	protected ServiceReference<ResponseTrackerFactory> responseTrackerFactoryServiceReference;
 
 	protected ResponseTrackerFactory responseTrackerFactory;
-
 
 	@Override
 	public final void start(final BundleContext bundleContext) throws Exception {
@@ -124,4 +129,21 @@ public abstract class PortalPluginBundleActivator implements BundleActivator {
 	}
 
 	protected abstract void doStop() throws Exception;
+
+	protected Injector createInjector(final Module... modules) {
+		return Guice.createInjector(new AbstractModule() {
+			@Override
+			protected void configure() {
+				bind(ReservationManager.class).toProvider(of(reservationManager));
+				bind(PortalEventBus.class).toProvider(of(portalEventBus));
+				bind(SessionManagement.class).toProvider(of(sessionManagement));
+				bind(SNAA.class).toProvider(of(snaa));
+				bind(RS.class).toProvider(of(rs));
+				bind(PluginContainer.class).toProvider(of(pluginContainer));
+				bind(DeviceDBService.class).toProvider(of(deviceDBService));
+				bind(ResponseTrackerFactory.class).toProvider(of(responseTrackerFactory));
+			}
+		}
+		).createChildInjector(modules);
+	}
 }
