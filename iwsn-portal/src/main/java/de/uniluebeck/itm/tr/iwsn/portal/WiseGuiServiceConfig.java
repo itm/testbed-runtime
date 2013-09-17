c0584d9da0601ecb60a05ba14126cc8692b5ceb7
Daniel Bimschas <daniel@bimschas.com>
2013-09-17 08:27:01 +0200

    Closing #326 (Allow to serve WiseGui from external directory)

--- iwsn-portal/src/main/java/de/uniluebeck/itm/tr/iwsn/portal/WiseGuiServiceConfig.java
+++ iwsn-portal/src/main/java/de/uniluebeck/itm/tr/iwsn/portal/WiseGuiServiceConfig.java
@@ -3,6 +3,9 @@ package de.uniluebeck.itm.tr.iwsn.portal;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import de.uniluebeck.itm.util.propconf.PropConf;
+import org.nnsoft.guice.rocoto.converters.FileConverter;
+
+import java.io.File;
 
 public class WiseGuiServiceConfig {
 
@@ -17,7 +20,22 @@ public class WiseGuiServiceConfig {
 	@Named(WISEGUI_CONTEXT_PATH)
 	private String wiseGuiContextPath;
 
+	@PropConf(
+			usage = "The directory under which to find the WiseGui. If not given WiseGui will be served from a packaged "
+					+ "internal version.",
+			typeConverter = FileConverter.class
+	)
+	public static final String WISEGUI_SOURCE_DIR = "wisegui.source_dir";
+
+	@Inject
+	@Named(WISEGUI_SOURCE_DIR)
+	private File wiseGuiSourceDir;
+
 	public String getWiseGuiContextPath() {
 		return wiseGuiContextPath;
+	}
+
+	public File getWiseGuiSourceDir() {
+		return wiseGuiSourceDir;
 	}
 }
