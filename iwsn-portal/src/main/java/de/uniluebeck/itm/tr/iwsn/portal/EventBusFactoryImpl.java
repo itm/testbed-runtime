82fcb9e080a77a8ea13168c7fa6d7f36bd9f56ed
Daniel Bimschas <daniel@bimschas.com>
2014-10-06 12:00:30 +0200

    Reverting PortalEventBus from async to sync as it used up to many threadys, potentially leading to OutOfMemoryErrors

--- iwsn-portal/src/main/java/de/uniluebeck/itm/tr/iwsn/portal/EventBusFactoryImpl.java
+++ iwsn-portal/src/main/java/de/uniluebeck/itm/tr/iwsn/portal/EventBusFactoryImpl.java
@@ -1,21 +1,11 @@
 package de.uniluebeck.itm.tr.iwsn.portal;
 
-import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
-import com.google.inject.Inject;
-import de.uniluebeck.itm.util.scheduler.SchedulerService;
 
 public class EventBusFactoryImpl implements EventBusFactory {
 
-	private final SchedulerService schedulerService;
-
-	@Inject
-	public EventBusFactoryImpl(final SchedulerService schedulerService) {
-		this.schedulerService = schedulerService;
-	}
-
 	@Override
 	public EventBus create(final String name) {
-		return new AsyncEventBus(name, schedulerService);
+		return new EventBus(name);
 	}
 }
