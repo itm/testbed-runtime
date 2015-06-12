5ca93d85376ec61f7451aad4b5022787421fe594
Daniel Bimschas <daniel@bimschas.com>
2015-06-12 15:14:34 +0200

    Working on #366: fixing compilation bug, incrementing to 0.10-SNAPSHOT

--- iwsn-federator/src/main/java/de/uniluebeck/itm/tr/federator/iwsn/FederatedReservationEventBus.java
+++ iwsn-federator/src/main/java/de/uniluebeck/itm/tr/federator/iwsn/FederatedReservationEventBus.java
@@ -3,15 +3,17 @@ package de.uniluebeck.itm.tr.federator.iwsn;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 import de.uniluebeck.itm.tr.iwsn.portal.EventBusFactory;
+import de.uniluebeck.itm.tr.iwsn.portal.PortalEventBus;
 import de.uniluebeck.itm.tr.iwsn.portal.Reservation;
 import de.uniluebeck.itm.tr.iwsn.portal.ReservationEventBusImpl;
 
 public class FederatedReservationEventBus extends ReservationEventBusImpl {
 
 	@Inject
-	public FederatedReservationEventBus(final EventBusFactory eventBusFactory,
+	public FederatedReservationEventBus(final PortalEventBus portalEventBus,
+										final EventBusFactory eventBusFactory,
 										@Assisted final Reservation reservation) {
-		super(eventBusFactory, reservation);
+		super(portalEventBus, eventBusFactory, reservation);
 	}
 
 }
